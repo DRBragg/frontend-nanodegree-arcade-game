3de8082006cef31968e0ace49b99b2a719520966
Drew Bragg <DRBragg@gmail.com>
2017-10-08 18:33:28 -0400

    working with objects

--- js/app.js
+++ js/app.js
@@ -29,15 +29,57 @@ Enemy.prototype.render = function() {
 
 // Player class
 var Player = function() {
-  this.sprite = 'image/char-boy.png'
+  // For Joella, Love Daddy
+  this.sprite = 'images/char-princess-girl.png'
   this.x = 200;
   this.y = 320;
 };
 
+// Handle player movement
+Player.prototype.handleInput = function(direction) {
+    if(direction == 'left' && this.x > 0) {
+        this.x -= 100;
+    } else if (direction == 'up' && this.y > 3) {
+      this.y -= 100;
+    } else if (direction == 'right' && this.x < 400) {
+      this.x += 100;
+    } else if (direction == 'down' && this.y < 400) {
+      this.y += 100;
+    }
+};
+
+// Check if player has reached water at every movement
+Player.prototype.update = function() {
+	if (this.y < 20) {
+    // If player reached water updated score and reset position
+    score++;
+    document.getElementById('score').innerHTML = score;
+    this.reset();
+  }
+};
+
+// Return player to SP
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 300;
+};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var score = 0;
+
 
 // Now instantiate your objects.
+var e1 = new Enemy(0, 50);
+var e2 = new Enemy(-220, 140);
+var e3 = new Enemy(-500, 230);
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [e1, e2, e3];
 // Place the player object in a variable called player
+var player = new Player();
 
 
 
